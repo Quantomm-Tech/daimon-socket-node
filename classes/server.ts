e8fc9833d033c0f1f987e7744acb9d9fe676f5e9
lrodriguezquantomm <lizeth.rodriguez@quantomm.tech>
2024-02-06 12:04:17 -0500

    Route msj estado de pago daimon beneficiarios

--- classes/server.ts
+++ classes/server.ts
@@ -1,10 +1,9 @@
-
-import express from 'express';
-import { SERVER_PORT } from '../global/environment';
-import socketIO from 'socket.io';
-import http from 'http';
-import * as socket from '../sockets/socket';
-import jwt from 'jsonwebtoken';
+import express from "express";
+import { SERVER_PORT } from "../global/environment";
+import socketIO from "socket.io";
+import http from "http";
+import * as socket from "../sockets/socket";
+import jwt from "jsonwebtoken";
 import jwksClient from "jwks-rsa";
 
 const client = jwksClient({
@@ -12,96 +11,89 @@ const client = jwksClient({
 });
 
 export default class Server {
+  private static _instance: Server;
+  public app: express.Application;
+  public port: number;
 
-    private static _instance : Server;
-    public app: express.Application;
-    public port: number;
+  public io: socketIO.Server;
 
-    public io: socketIO.Server;
+  private httpServer: http.Server;
 
-    private httpServer: http.Server;
+  private constructor() {
+    this.app = express();
+    this.port = SERVER_PORT;
 
-    private constructor() {
+    this.httpServer = new http.Server(this.app);
+    this.io = new socketIO.Server(this.httpServer, {
+      cors: {
+        origin: "*",
+      },
+    });
 
-        
-        this.app = express();
-        this.port = SERVER_PORT;
+    this.app.set("io", this.io);
 
-        this.httpServer = new http.Server( this.app );
-        this.io = new socketIO.Server(this.httpServer,{
-            cors: {
-              origin: '*'
-            }});
+    this.listenSockets();
+  }
 
-        
-        this.app.set("io", this.io);
+  public static get instance() {
+    return this._instance || (this._instance = new this());
+  }
 
-        this.listenSockets();
-            
-        
-    }
+  private listenSockets() {
+    this.io.use((socket, next) => {
+      console.log("socket.handshake: ", socket.handshake);
+      const token = socket.handshake.auth.token;
 
-    public static get instance(){
-        return this._instance || ( this._instance = new this() );
-    }
+      if (!token) {
+        const error = new Error(
+          "Token de autenticación no proporcionado desde la app"
+        );
+        console.error(error);
+        return next(error);
+      }
 
-    private listenSockets() {
-
-        this.io.use((socket, next) => {
-            
-            const token = socket.handshake.auth.token;
-            
-            if (!token) {
-              const error = new Error('Token de autenticación no proporcionado desde la app');
-              console.error(error);
-              return next(error);
-            }
-
-            function getKey(header: any, callback: any) {
-              client.getSigningKey(header.kid, function (err: any, key: any) {
-                  if (err) {                    
-                      return next(new Error('Error de Signingkey'));
-                  }
-                  const signingKey = key.publicKey || key.rsaPublicKey;
-                  callback(null, signingKey);
-              });
-            }
-
-            // Verificar y decodificar el token JWT
-            jwt.verify(token, getKey, (err: any) => {
-              if (err) {
-                console.error('Error de autenticación: ', err)
-                // El token no es válido, rechazar la conexión
-                return next(new Error('Error de autenticación'));
-              }
-        
-              next();
-            });
+      function getKey(header: any, callback: any) {
+        client.getSigningKey(header.kid, function (err: any, key: any) {
+          if (err) {
+            return next(new Error("Error de Signingkey11"));
+          }
+          const signingKey = key.publicKey || key.rsaPublicKey;
+          callback(null, signingKey);
         });
-      
-        this.io.on("connection", (client) => {
-          console.log("New Client connected: ", client.id);
+      }
 
-          // Mensajes
-          socket.mensaje( client ,this.io);
+      // // Verificar y decodificar el token JWT
+      // jwt.verify(token, getKey, (err: any) => {
+      //   if (err) {
+      //     console.error("Error de autenticación: ", err);
+      //     // El token no es válido, rechazar la conexión
+      //     return next(new Error("Error de autenticación"));
+      //   }
 
-          // Desconectar
-          socket.disconnect( client );
+      //   next();
+      // });
 
-          //this.discconectAll()
+      next();
+    });
 
-        });
-    }
+    this.io.on("connection", (client) => {
+      console.log("New Client connected: ", client.id);
 
-    
-    private discconectAll() {
-      this.io.disconnectSockets()
-    }
+      // Mensajes
+      socket.mensaje(client, this.io);
 
-    start( callback: Function ) {
+      // Desconectar
+      socket.disconnect(client);
 
-        this.httpServer.listen( this.port, callback() );
+      //this.discconectAll()
+    });
+  }
 
-    }
+  private discconectAll() {
+    this.io.disconnectSockets();
+  }
 
+  start(callback: Function) {
+    this.httpServer.listen(this.port, callback());
+  }
 }
--- routes/router.ts
+++ routes/router.ts
@@ -1,89 +1,92 @@
-
-import { Router, Request, Response } from 'express';
-import { authMiddleware } from './authMiddleware';
+import { Router, Request, Response } from "express";
+import { authMiddleware } from "./authMiddleware";
 
 const router = Router();
 
-router.get('/mensajes', ( req: Request, res: Response  ) => {
-
-    res.json({
-        ok: true,
-        mensaje: 'Todo esta bien el servicio funciona!!'
-    });
-
+router.get("/mensajes", (req: Request, res: Response) => {
+  res.json({
+    ok: true,
+    mensaje: "Todo esta bien el servicio funciona!!",
+  });
 });
 
-router.post('/mensajes', authMiddleware, ( req: Request, res: Response  ) => {
+// router.post("/mensajes", authMiddleware, (req: Request, res: Response) => {
+router.post("/mensajes", (req: Request, res: Response) => {
+  const cuerpo = req.body.cuerpo;
+  const de = req.body.de;
 
-    const cuerpo = req.body.cuerpo;
-    const de     = req.body.de;
+  // Enviar mensaje al cliente
+  const io = req.app.get("io");
 
-    // Enviar mensaje al cliente
-    const io = req.app.get('io')
+  if (io) {
+    console.log("Se va emitir ...");
+    io.emit("message", req.body);
+  }
 
+  res.json({
+    ok: true,
+    cuerpo,
+    de,
+  });
+});
+
+router.post("/mensajes/:id", (req: Request, res: Response) => {
+  const cuerpo = req.body.cuerpo;
+  const de = req.body.de;
+  const id = req.params.id;
+
+  res.json({
+    ok: true,
+    cuerpo,
+    de,
+    id,
+  });
+});
+
+router.post("/notification/:companyId", (req: Request, res: Response) => {
+  const companyId = req.params.companyId;
+
+  const notificationType = req.body.notificationType;
+
+  if (notificationType && companyId) {
+    const io = req.app.get("io");
     if (io) {
-        console.log('Se va emitir ...')
-        io.emit('message', req.body)
+      console.log(`Se va emitir a: notification-${companyId}`);
+      io.emit(`notification-${companyId}`, req.body);
     }
-        
 
     res.json({
-        ok: true,
-        cuerpo,
-        de
+      isSuccess: true,
+      data: req.body,
     });
-
+  } else {
+    res.json({
+      isSuccess: false,
+      error: "Bad request",
+    });
+  }
 });
 
+router.post("/paystate/:beneficiaryId", (req: Request, res: Response) => {
+  const beneficiaryId = req.params.beneficiaryId;
 
-router.post('/mensajes/:id', ( req: Request, res: Response  ) => {
-
-    const cuerpo = req.body.cuerpo;
-    const de     = req.body.de;
-    const id     = req.params.id;
-
+  if (beneficiaryId) {
+    const io = req.app.get("io");
+    if (io) {
+      console.log(`Se va emitir a: paystate-${beneficiaryId}`);
+      io.emit(`paystate-${beneficiaryId}`, req.body);
+    }
 
     res.json({
-        ok: true,
-        cuerpo,
-        de,
-        id
+      isSuccess: true,
+      data: req.body,
     });
-
-});
-
-
-router.post('/notification/:companyId', ( req: Request, res: Response  ) => {
-
-    const companyId = req.params.companyId;
-
-    const notificationType = req.body.notificationType
-
-    if (notificationType && companyId) {
-        const io = req.app.get('io')
-        if (io ) {
-            console.log(`Se va emitir a: notification-${companyId}`)
-            io.emit(`notification-${companyId}`, req.body)
-        }
-
-        res.json({
-            isSuccess: true,
-            data: req.body
-        });
-
-    } else {
-        res.json({
-            isSuccess: false,
-            error: 'Bad request'
-        });
-    }
-    
-
-        
-
-
+  } else {
+    res.json({
+      isSuccess: false,
+      error: "Bad request",
+    });
+  }
 });
 
 export default router;
-
-
